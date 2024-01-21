d269b1cb20dd0236e2a6d111ba7985c051d5eaa7
Mason Hicks <mhicks071@yahoo.com>
2024-01-21 13:54:15 -0500

    Wrote tests for string crypto module

---
+++ security/stringCrypto_test.go
@@ -0,0 +1,59 @@
+package security
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestEncryptionDecryption(t *testing.T) {
+	// Sample data
+	initial := "The quick brown fox jumps over the lazy dog, and a cat watches."
+	key := []byte("TEST KEY 1")
+
+	// Encrypted and decrypt sample data
+	text, err := encrypt(initial, key)
+	if err != nil {
+		t.Fatalf("encryption failed")
+	}
+
+	text, err = decrypt(text, key)
+	if err != nil {
+		t.Fatalf("decryption failed")
+	}
+
+	// Ensure correct behavior
+	if strings.Compare(initial, text) != 0 {
+		t.Fatalf("final and initial strings differ")
+	}
+}
+
+func TestEncryption(t *testing.T) {
+	// Sample data
+	initial := "The quick brown fox jumps over the lazy dog, and a cat watches."
+	key1 := []byte("TEST KEY 1")
+	key2 := []byte("TEST KEY 2")
+
+	// Encrypt sample data
+	text1, err := encrypt(initial, key1)
+	if err != nil {
+		t.Fatalf("encryption 1 failed")
+	}
+
+	text2, err := encrypt(initial, key2)
+	if err != nil {
+		t.Fatalf("encryption 2 failed")
+	}
+
+	text3, err := encrypt(initial, key1)
+	if err != nil {
+		t.Fatalf("encryption 3 failed")
+	}
+
+	// Ensure correct behavior
+	if strings.Compare(text1, text2) == 0 {
+		t.Fatalf("separate keys encrypt to same string")
+	}
+	if strings.Compare(text1, text3) == 0 {
+		t.Fatalf("same key encrypts to separate strings")
+	}
+}
