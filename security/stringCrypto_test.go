55b21da5844c5b084d6a00cad72ab461250ef242
Mason Hicks <mhicks071@yahoo.com>
2024-01-21 14:03:41 -0500

    Added extra test for string cryptography

--- security/stringCrypto_test.go
+++ security/stringCrypto_test.go
@@ -57,3 +57,23 @@ func TestEncryption(t *testing.T) {
 		t.Fatalf("same key encrypts to separate strings")
 	}
 }
+
+func TestDecryption(t *testing.T) {
+	// Sample data
+	initial := "The quick brown fox jumps over the lazy dog, and a cat watches."
+	key1 := []byte("TEST KEY 0000001")
+	key2 := []byte("TEST KEY 0000002")
+
+	// Encrypt sample data
+	text1, _ := Encrypt(initial, key1)
+	text2, _ := Encrypt(initial, key2)
+
+	// Decrypt sample data
+	text1, _ = Decrypt(initial, key2)
+	text2, _ = Decrypt(initial, key1)
+
+	// Ensure correct behavior
+	if strings.Compare(initial, text1) == 0 || strings.Compare(initial, text2) == 0 {
+		t.Fatalf("different key decrypts encrypted string")
+	}
+}
